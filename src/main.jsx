0f5988a1646943a188733671c08ccb91b12baaae
VikingMorich <40545669+VikingMorich@users.noreply.github.com>
2025-05-23 11:01:01 +0200

    Add home and admin view

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import { Home } from './views/Home/Home.jsx';
+import { Admin } from './views/Admin/Admin.jsx';
+// import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
---
+++ src/views/Admin/Admin.jsx
@@ -0,0 +1,1 @@
+export const Admin = () => <div>ADMIN</div>
