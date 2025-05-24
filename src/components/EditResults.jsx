7fb457eea149abc66b60c15b820c3dcb8aad382e
VikingMorich <40545669+VikingMorich@users.noreply.github.com>
2025-05-24 13:28:26 +0200

    view home info

---
+++ src/components/EditResults.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+export const EditResults = () => {
+  return (
+    <div>
+      <div>
+        <h2>Editar Resultats</h2>
+      </div>
+    </div>
+  );
+};
